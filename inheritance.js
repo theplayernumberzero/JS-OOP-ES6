a79edd0aab02445094fb569306c698bc8f1497ea
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2023-11-26 18:51:59 +0300

    lesson 3

---
+++ inheritance.js
@@ -0,0 +1,24 @@
+class Person{   //Superclass, Baseclass
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+    showInfos(){
+        console.log("İsim: "+this.name+" Yas: "+this.age);
+    }
+}
+
+class Employee extends Person{      //Subclass, derivedClass, Childclass
+    constructor(name,age){
+        super(name,age);    //this.name ve this.age i yeniden yazmana gerek kalmıyor.
+        this.salary = this.salary;
+    }
+
+    showInfos(){    //Override
+        console.log("İsim: "+this.name+" Yas: "+this.age+" Maas: "+this.salary);
+    }
+}
+
+const emp = new Employee("Bahadir",22,0);
+
+emp.showInfos();
